7ffea9b4b06cc9ebb9b9c5e4c2f90d5c2789aa01
lahfaouti <ilahfaouti@gmail.com>
2024-12-13 13:42:32 +0100

    Mise à jour de next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  experimental: {
-    serverActions: true, // Activer les Server Actions
+  images: {
+    unoptimized: true,
   },
-  output: 'export', // Exportation statique du projet
-  basePath: '/PORTFOLIO_LAHFAOUTI', // Remplacer par ton nom de projet sur GitHub
-  assetPrefix: '/PORTFOLIO_LAHFAOUTI', // Remplacer par ton nom de projet sur GitHub
 };
